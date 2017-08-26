086257e4b48a75f0e068674d2bbccad0345ce0b0
SanJiang <changsanjiang@gmail.com>
2017-08-26 19:27:58 +0800

    Update

--- SJVideoPlayerProject/SJVideoPlayerProject/VideoPlayerViewController.m
+++ SJVideoPlayerProject/SJVideoPlayerProject/VideoPlayerViewController.m
@@ -29,9 +29,18 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    // MARK: Player View
+
     SJVideoPlayer *player = [SJVideoPlayer sharedPlayer];
     [self.view addSubview:player.view];
+    [player.view mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.offset(64);
+        make.leading.trailing.offset(0);
+        make.height.equalTo(player.view.mas_width).multipliedBy(9.0 / 16.0);
+    }];
+
     
+    // MARK: AssetURL
     
 //    player.assetURL = [[NSBundle mainBundle] URLForResource:@"sample.mp4" withExtension:nil];
 //    player.assetURL = [NSURL URLWithString:@"http://streaming.youku.com/live2play/gtvyxjj_yk720.m3u8?auth_key=1525831956-0-0-4ec52cd453761e1e7f551decbb3eee6d"];
@@ -40,19 +49,18 @@
     
 //    player.assetURL = [NSURL URLWithString:@"http://vod.lanwuzhe.com/9da7002189d34b60bbf82ac743241a61/d0539e7be21a4f8faa9fef69a67bc1fb-5287d2089db37e62345123a1be272f8b.mp4?video="];
     
-    [player.view mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.offset(64);
-        make.leading.trailing.offset(0);
-        make.height.equalTo(player.view.mas_width).multipliedBy(9.0 / 16.0);
-    }];
     
     
+    // MARK: Loading Placeholder
     
     player.placeholder = [UIImage imageNamed:@"sj_video_player_placeholder"];
     
+
     
+    // MARK: More Settings
+
     SJVideoPlayerMoreSetting.titleFontSize = 12;
-    
+
     SJVideoPlayerMoreSetting *model0 = [[SJVideoPlayerMoreSetting alloc] initWithTitle:@"点赞" image:[UIImage imageNamed:@"db_video_like_n"] clickedExeBlock:^(SJVideoPlayerMoreSetting * _Nonnull model) {
         NSLog(@"clicked %@", model.title);
         [[SJVideoPlayer sharedPlayer] showTitle:@"超长震古烁今的名字"];
@@ -74,6 +82,9 @@
     }];
     
     player.moreSettings = @[model0, model1, model2, model3];
+    
+    
+    // MARK: Clicked Back Button
     
     __weak typeof(self) _self = self;
     player.clickedBackEvent = ^{

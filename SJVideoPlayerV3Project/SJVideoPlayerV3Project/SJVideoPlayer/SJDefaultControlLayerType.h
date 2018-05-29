499604c907889c59bf536f2e037de71aa24d09e5
changsanjiang <changsanjiang@gmail.com>
2018-05-29 21:41:34 +0800

    切换器思路待续

---
+++ SJVideoPlayerV3Project/SJVideoPlayerV3Project/SJVideoPlayer/SJDefaultControlLayerType.h
@@ -0,0 +1,18 @@
+//
+//  SJDefaultControlLayerType.h
+//  SJVideoPlayerV3Project
+//
+//  Created by 畅三江 on 2018/5/29.
+//  Copyright © 2018年 畅三江. All rights reserved.
+//
+
+#ifndef SJDefaultControlLayerType_h
+#define SJDefaultControlLayerType_h
+
+typedef NS_ENUM(NSUInteger, SJDefaultControlLayerType) {
+    SJDefaultControlLayerType_Edge,
+    SJDefaultControlLayerType_DraggingPreview,
+    SJDefaultControlLayerType,
+};
+
+#endif /* SJDefaultControlLayerType_h */
--- SJVideoPlayerV3Project/SJVideoPlayerV3Project/SJVideoPlayer/SJVideoPlayerSwitcher.h
+++ SJVideoPlayerV3Project/SJVideoPlayerV3Project/SJVideoPlayer/SJVideoPlayerSwitcher.h
@@ -15,8 +15,19 @@ NS_ASSUME_NONNULL_BEGIN
  比如:
  切换到GIF控制层,  就设置 baseVideoPlayer.delegate = GIF控制层
  切换到导出视频的控制层, 就设置 baseVideoPlayer.delegate = 导出视频控制层
+ 
+ 1. 控制层告诉`切换器`需要切换控制层了
+ 
+ 2. `切换器`通知`delegate`切换控制层
+ 
+ 3. `delegate`选择好一个控制层交个`切换器` 进行切换
+ 
+ 
+ 1. `控制层`怎么告诉`切换器` .....
  */
 @protocol SJVideoPlayerSwitcherDelegate <NSObject>
+
+
 
 @end
 

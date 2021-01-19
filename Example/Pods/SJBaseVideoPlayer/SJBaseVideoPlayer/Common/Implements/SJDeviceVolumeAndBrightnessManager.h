268adbdd15e60dcf821999524497aeb54dc5137f
shafujiu <zhaowei200711@163.com>
2021-01-19 22:13:41 +0800

    开发api 自定义 声音控制，亮度控制条（原有接口保留）

--- Example/Pods/SJBaseVideoPlayer/SJBaseVideoPlayer/Common/Implements/SJDeviceVolumeAndBrightnessManager.h
+++ Example/Pods/SJBaseVideoPlayer/SJBaseVideoPlayer/Common/Implements/SJDeviceVolumeAndBrightnessManager.h
@@ -10,9 +10,36 @@
 #import "SJDeviceVolumeAndBrightnessManagerDefines.h"
 
 NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark - SJDeviceOutputPromptView
+@protocol SJDeviceOutputPromptViewDataSource <NSObject>
+
+/// 普通状态
+@property (nonatomic, strong, nullable) UIImage *image;
+/// 其实状态
+@property (nonatomic, strong, nullable) UIImage *startImage;
+
+@property (nonatomic, assign) float progress;
+@property (nonatomic, strong, nullable) UIColor *traceColor;
+@property (nonatomic, strong, nullable) UIColor *trackColor;
+@end
+
+@protocol SJDeviceOutputPromptView <NSObject>
+@property (nonatomic, strong) id<SJDeviceOutputPromptViewDataSource> dataSource;
+
+- (void)refreshData;
+@end
+
+
+#pragma mark -
+
 @interface SJDeviceVolumeAndBrightnessManager : NSObject<SJDeviceVolumeAndBrightnessManager>
 + (instancetype)shared;
 
+@property (nonatomic, strong, nullable) id<SJDeviceOutputPromptView> brightnessView;
+@property (nonatomic, strong, nullable) id<SJDeviceOutputPromptView> volumeView;
+
+/// 以下属性 优先于 brightnessView，volumeView中的dataSource的配置
 @property (nonatomic, strong, null_resettable) UIColor *traceColor;
 @property (nonatomic, strong, null_resettable) UIColor *trackColor;
 
